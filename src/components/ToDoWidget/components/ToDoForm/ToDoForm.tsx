563ba875c3c179ee7113e0aa508b774615317b55
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2022-10-31 20:22:39 +0200

    feat: complit app

--- src/components/ToDoWidget/components/ToDoForm/ToDoForm.tsx
+++ src/components/ToDoWidget/components/ToDoForm/ToDoForm.tsx
@@ -2,48 +2,41 @@ import { ChangeEvent, FC, FormEvent, useState } from 'react';
 import cx from 'classnames';
 import styles from './ToDoForm.module.css';
 import { IToDoFormProps } from './ToDoForm.props';
-import { nanoid } from 'nanoid';
 
 
-const ToDoForm: FC<IToDoFormProps> = ({ setToDo ,className, ...props }) => {
+const ToDoForm: FC<IToDoFormProps> = ({ addToDo, className, ...props }) => {
   const [inputValue, setInputValue] = useState<string>('');
 
   const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-  }
+  };
 
-  const submitHandler = (e: FormEvent) => {
-    e.preventDefault()
-    if(inputValue) {
-      setToDo((prevState) => [
-      {
-        label: inputValue,
-        completed: false,
-        id: nanoid(),
-      },
-      ...prevState,
-    ]);
+  const formSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    addToDo(inputValue);
     setInputValue('');
-    }
   }
 
   return (
-  <form className={cx(styles.form, className)} {...props} onSubmit={submitHandler}>
-    <label htmlFor='toDoInput'>
-      <input
-        type='text'
-        id='toDoInput'
-        name='toDoInput'
-        value={inputValue}
-        className={styles.input}
-        placeholder='What needs to be done?'
-        onChange={changeHandler}
-      />
-    </label>
-    {inputValue && <button type='submit' className={styles.button} disabled={!inputValue}>
-      &crarr;
-    </button>}
-  </form>
-);}
+    <form className={cx(styles.form, className)} {...props} onSubmit={formSubmitHandler}>
+      <label htmlFor='toDoInput'>
+        <input
+          type='text'
+          id='toDoInput'
+          name='toDoInput'
+          value={inputValue}
+          className={styles.input}
+          placeholder='What needs to be done?'
+          onChange={changeHandler}
+        />
+      </label>
+      {inputValue && (
+        <button type='submit' className={styles.button} disabled={!inputValue}>
+          &crarr;
+        </button>
+      )}
+    </form>
+  );
+};
 
 export default ToDoForm;
